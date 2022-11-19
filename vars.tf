9f9a73f8e6511b184d56a3f315fc11b50f5f4c00
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-19 10:07:17 -0700

    Updating

--- vars.tf
+++ vars.tf
@@ -1,2 +1,9 @@
 variable "elasticache" {}
 variable "env" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "name" {}
+variable "subnets" {}
+
